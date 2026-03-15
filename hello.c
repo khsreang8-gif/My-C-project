5c053a0acfb0b257a488d11ad1a6b0de3c9842d4
Khsreang <khsreang8@gmail.com>
2026-03-15 18:32:59 +0900

    Add if,else.c

--- hello.c
+++ hello.c
@@ -1,12 +1,78 @@
 #include <stdio.h>
 int main() {
+	
+/*	lesson 1
 	int age = 20;
 	float height = 1.70;
 	char grade = 'A';
 
 	printf("Age: %d\n", age);
 	printf("Height: %.2f\n", height);
-	printf("Grade: %c\n", grade);
+	printf("Grade: %c\n", grade);*/
+	
+/*	lesson 2
+	
+	int age;
+	float height;
+	char grade;
+	
+	printf("Enter age: ");
+	scanf("%d", &age);
+	
+	printf("Enter your height: ");
+	scanf("%f", &height);
+	
+	printf("Enter grade: ");
+	scanf(" %c", &grade);
+	
+	printf("\nAge: %d", age);
+	printf("\nHeight: %.2f", height);
+	printf("\nGrade: %c", grade);*/
+	
+/*	lesson 3
+	
+		int age;
+	
+	printf("Enter your age: ");
+	scanf("%d", &age);
+	
+	if(age >= 18) {
+		printf("You are adult");
+		}
+		
+		else {
+			printf("You are child");
+		}*/
+		
+		/*	lesson 4
+		
+		#include <stdio.h>
+
+int main() {
+	
+/*int i;
+	for(i = 1; i <= 5; i++) {
+		printf("Number: %d\n", i);
+		
+	}*/
+	
+/*	int i;
+	for (i = 0; i <= 10; i++) {
+		printf("Number: %d\n", i);
+	}*/
+	
+/*	int i;
+	
+	for (i = 1; i <= 3; i++ ) {
+		printf("Number: %d\n", i);
+	}*/
+	
+/*	int i;
+	
+	for (i = 0; i <= 10; i = i+2) {
+		printf("Number: %d\n", i);
+	}*/
+	
 	
 	getchar();
 	return 0;
---
+++ loop.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+	
+/*int i;
+	for(i = 1; i <= 5; i++) {
+		printf("Number: %d\n", i);
+		
+	}*/
+	
+/*	int i;
+	for (i = 0; i <= 10; i++) {
+		printf("Number: %d\n", i);
+	}*/
+	
+/*	int i;
+	
+	for (i = 1; i <= 3; i++ ) {
+		printf("Number: %d\n", i);
+	}*/
+	
+/*	int i;
+	
+	for (i = 0; i <= 10; i = i+2) {
+		printf("Number: %d\n", i);
+	}*/
+	return 0;
+	
+}
